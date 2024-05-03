e7f9e91e6262b0e50858240f8a81fedff55498a0
Armin Biere <armin.biere@gmail.com>
2024-05-03 16:05:46 +0200

    comments

--- asreti.c
+++ asreti.c
@@ -9,15 +9,21 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+// We read a ReTI assembler file.
+
 static size_t lineno = 1;
 static int last_read_char;
 static const char *assembler_path;
 static bool close_assembler_file;
 static FILE *assembler_file;
 
+// And write binary encoded ReTI code file.
+
 static const char *code_path;
 static bool close_code_file;
 static FILE *code_file;
+
+// Two generic error functions.
 
 static void die(const char *, ...) __attribute__((format(printf, 1, 2)));
 
@@ -44,10 +50,25 @@ static void error(const char *fmt, ...) {
   exit(1);
 }
 
+// Four factored out short-cuts to common parse error.
+
+static void invalid_instruction() { error("invalid instruction"); }
+
+static void invalid_source() { error("invalid source register"); }
+
+static void invalid_destination() { error("invalid destination register"); }
+
+static void invalid_immediate() { error("invalid immediate"); }
+
+// Check whether the given path points to a file.
+
 static bool file_exists(const char *path) {
   struct stat buf;
   return !stat(path, &buf);
 }
+
+// Read from the assembler file, handle DOS/Windows carriage return and
+// update line number counter 'lineno'.
 
 static int read_char(void) {
   int res = getc(assembler_file);
@@ -62,7 +83,7 @@ static int read_char(void) {
   return res;
 }
 
-static void invalid_instruction() { error("invalid instruction"); }
+// Allows compile time constants for bit-vectors (6-bit prefix of machine code).
 
 #define CODE(A, B, C, D, E, F)                                                 \
   ((((unsigned)(A)) << 31) | (((unsigned)(B)) << 30) |                         \
@@ -70,6 +91,8 @@ static void invalid_instruction() { error("invalid instruction"); }
    (((unsigned)(E)) << 27) | (((unsigned)(F)) << 26))
 
 // clang-format off
+
+// This enumeration has all our 6-bit most-significant prefix code-words.
 
 enum code {
   LOAD =     CODE(0,1,0,0,0,0),
@@ -104,6 +127,8 @@ enum code {
 
 int main(int argc, char **argv) {
 
+  // Command line option parsing.
+
   for (int i = 1; i != argc; i++) {
     const char *arg = argv[i];
     if (!strcmp(arg, "-h") || !strcmp(arg, "--help")) {
@@ -117,6 +142,8 @@ int main(int argc, char **argv) {
       code_path = arg;
   }
 
+  // Open and read assembler file.
+
   if (!assembler_path)
     assembler_path = "<stdin>", assembler_file = stdin;
   else if (!file_exists(assembler_path))
@@ -126,30 +153,68 @@ int main(int argc, char **argv) {
   else
     close_assembler_file = true;
 
-  if (!code_path)
+  // Open and write code file.
+
+  if (!code_path && isatty(2))
+    die("will not write binary code to terminal");
+  else if (!code_path)
     code_path = "<stdout>", code_file = stdout;
   else if (!(code_file = fopen(code_path, "w")))
     die("could not write code file '%s'", code_path);
   else
     close_code_file = true;
 
+  // This loops reads assembler instructions from the assembler file and
+  // writes them in a single pass to the output code file.
+
   for (;;) {
+
     int ch = read_char();
-    bool parse_source = false;
-    bool parse_destination = true;
-    bool parse_immediate = true;
+
+    // These flags determine after parsing the name of the
+    // instruction whether we need to read 'S', 'D' and 'i'.
+
+    bool parse_source = false;	   // Only for 'MOVE' necessary.
+    bool parse_destination = true; // Most instructions require 'D'.
+    bool parse_immediate = true;   // Most instructions require 'i'.
+
+    // This word accumulates the machine code of the parsed instruction.
+
     unsigned code = NOP;
+
     switch (ch) {
+
     case ' ':
     case '\t':
-      continue;
+      continue; // Skip white space at the beginning of the line.
+
     case EOF:
-      goto DONE;
+      goto DONE; // Terminate if end-of-file is reached.
+
+      // Full line comments start with ';'.
+
     case ';':
       while ((ch = read_char()) != '\n')
 	if (ch == EOF)
 	  error("unexpected end-of-file in comment");
       continue;
+
+      // Two specific error situations.
+
+    case '\n':
+      error("unexpected empty line");
+      break;
+
+    default:
+      if (isprint(ch))
+	error("unexpected character '%c'", ch);
+      else
+	error("unexpected character code '0x%02x'", ch);
+      break;
+
+      // The remaining parsing is done alphabetically with respect to the
+      // first character of the instruction read.
+
     case 'A':
       ch = read_char();
       if (ch == 'D') {
@@ -181,6 +246,7 @@ int main(int argc, char **argv) {
       } else
 	invalid_instruction();
       break;
+
     case 'J':
       for (const char *p = "UMP"; *p; p++)
 	if (*p != read_char())
@@ -219,6 +285,7 @@ int main(int argc, char **argv) {
 	invalid_instruction();
       parse_destination = false;
       break;
+
     case 'L':
       for (const char *p = "OAD"; *p; p++)
 	if (*p != read_char())
@@ -247,6 +314,7 @@ int main(int argc, char **argv) {
       } else
 	invalid_instruction();
       break;
+
     case 'M':
       for (const char *p = "OVE"; *p; p++)
 	if (*p != read_char())
@@ -256,14 +324,17 @@ int main(int argc, char **argv) {
       parse_immediate = false;
       ch = read_char();
       break;
+
     case 'N':
       for (const char *p = "OP"; *p; p++)
 	if (*p != read_char())
 	  invalid_instruction();
       code = NOP;
+      ch = read_char();
       parse_destination = false;
       parse_immediate = false;
       break;
+
     case 'O':
       ch = read_char();
       if (ch == 'P') {
@@ -290,6 +361,7 @@ int main(int argc, char **argv) {
       } else
 	invalid_instruction();
       break;
+
     case 'S':
       ch = read_char();
       if (ch == 'T') {
@@ -331,35 +403,134 @@ int main(int argc, char **argv) {
       } else
 	invalid_instruction();
       break;
-    case '\n':
-      error("unexpected empty line");
-      break;
-    default:
-      if (isprint(ch))
-	error("unexpected character '%c'", ch);
-      else
-	error("unexpected character code '0x%02x'", ch);
-      break;
     }
 
-    if (parse_source) {
+    // After parsing the prefix the instruction and setting its code we
+    // parse the remaining parts of an instruction ('S', 'D' and 'i').
+
+    if (parse_source) { // Parse source register 'S'.
       if (ch != ' ')
 	invalid_instruction();
       assert(code == MOVE);
+      unsigned S = 0;
+      ch = read_char();
+      if (ch == 'A') {
+	for (const char *p = "CC"; *p; p++)
+	  if (*p != read_char())
+	    invalid_source();
+	S = 3;
+      } else if (ch == 'I') {
+	ch = read_char();
+	if (ch != 'N')
+	  invalid_source();
+	ch = read_char();
+	if (ch == '1')
+	  S = 1;
+	else if (ch == '2')
+	  S = 2;
+	else
+	  invalid_source();
+      } else if (ch == 'P') {
+	if (read_char() != 'C')
+	  invalid_source();
+	assert(!S);
+      } else
+	invalid_source();
+      code |= S << 26;
+      ch = read_char();
     }
 
-    if (parse_destination) {
-      if (ch != ' ')
-	invalid_instruction();
+    if (parse_destination) { // Parse destination register 'D'.
+      if (ch != ' ') {
+	if (parse_source)
+	  invalid_source();
+	else
+	  invalid_instruction();
+      }
+      unsigned D = 0;
+      ch = read_char();
+      if (ch == 'A') {
+	for (const char *p = "CC"; *p; p++)
+	  if (*p != read_char())
+	    invalid_destination();
+	D = 3;
+      } else if (ch == 'I') {
+	ch = read_char();
+	if (ch != 'N')
+	  invalid_destination();
+	ch = read_char();
+	if (ch == '1')
+	  D = 1;
+	else if (ch == '2')
+	  D = 2;
+	else
+	  invalid_destination();
+      } else if (ch == 'P') {
+	if (read_char() != 'C')
+	  invalid_destination();
+	assert(!D);
+      } else
+	invalid_source();
+      code |= D << 24;
+      ch = read_char();
     }
 
-    if (parse_immediate) {
-      if (ch != ' ')
-	invalid_instruction();
+    if (parse_immediate) { // Parse immediate 'i'.
+      if (ch != ' ') {
+	if (parse_destination)
+	  invalid_destination();
+	else {
+	  assert(!parse_source);
+	  invalid_instruction();
+	}
+      }
+      int sign;
+      ch = read_char();
+      if (ch == '-') {
+	sign = -1;
+	ch = read_char();
+	if (ch == '0')
+	  invalid_immediate();
+      } else
+	sign = 1;
+      if (!isdigit(ch))
+	invalid_immediate();
+      unsigned i = (ch - '0');
+      const unsigned max_immediate = 0xffffff;
+      while (isdigit(ch = read_char())) {
+	if (max_immediate / 10 < i)
+	  invalid_immediate();
+	i *= 10;
+	int digit = ch - '0';
+	if (max_immediate - digit < i)
+	  i += digit;
+      }
+      assert(i <= max_immediate);
+      if (sign < 0) {
+	const unsigned max_signed_immediate = 0x800000;
+	if (i > max_signed_immediate)
+	  invalid_immediate();
+	i = (~i + 1) & max_immediate;
+      }
+      assert(i <= max_immediate);
+      code |= i;
     }
+
+    if (ch != ' ' && ch != '\t' && ch != '\n') {
+      if (parse_immediate)
+	invalid_immediate();
+      else if (parse_destination)
+	invalid_destination();
+      else
+	invalid_source();
+    }
+
+    // Skip white space after a complete instruction.
 
     while (ch == ' ' || ch == '\t')
       ch = read_char();
+
+    // Skip comments after an instruction.
 
     if (ch == ';') {
       while ((ch = read_char()) != '\n')
@@ -369,11 +540,21 @@ int main(int argc, char **argv) {
 
     if (ch != '\n')
       error("expected new-line");
+
+    // Write the word in little endian encoding to the code file.
+
+    for (unsigned byte = 0; byte != 4; byte++) {
+      const unsigned shift = byte * 8;
+      unsigned char ch = code >> shift;
+      fputc(ch, code_file);
+    }
   }
 
 DONE:
+
   if (close_assembler_file)
     fclose(assembler_file);
+
   if (close_code_file)
     fclose(code_file);
 
